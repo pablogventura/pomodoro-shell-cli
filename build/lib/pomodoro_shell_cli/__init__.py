ea2f6c524abc94319baa0dbf3b446e3bcba9de30
Pablo Ventura <pablogventura@gmail.com>
2026-02-03 01:13:17 -0300

    Remove build artifacts and egg-info from tracking

--- build/lib/pomodoro_shell_cli/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Pomodoro Shell CLI - Command-line interface for GNOME Pomodoro."""
-
-__version__ = "0.1.0"
--- build/lib/pomodoro_shell_cli/__main__.py
+++
@@ -1,6 +0,0 @@
-"""Allow running as python -m pomodoro_shell_cli."""
-
-from pomodoro_shell_cli.cli import main
-
-if __name__ == '__main__':
-    main()
